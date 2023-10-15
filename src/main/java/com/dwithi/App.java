a2fa4b315cd5bd097923559274e739aabe880bc6
manekalyani2 <manekalyani2@gmail.com>
2023-10-15 11:26:35 +0530

    initial commit

---
+++ src/main/java/com/dwithi/App.java
@@ -0,0 +1,14 @@
+
+package com.dwithi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
